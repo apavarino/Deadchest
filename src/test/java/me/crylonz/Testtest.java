4a1ee69a1405b660842b308a75ab46b460b6cfda
Arthur Pavarino <5781079+apavarino@users.noreply.github.com>
2020-04-09 22:25:32 +0200

    update for CI

---
+++ src/test/java/me/crylonz/Testtest.java
@@ -0,0 +1,13 @@
+package me.crylonz;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class Testtest {
+
+    @Test
+    public void testLucky() {
+        assertEquals(7, 7);
+    }
+}
