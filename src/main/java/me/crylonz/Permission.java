14161d54a0f1155c1d8c6dce88845f0aab83fb16
ldemoulins <louis.demoulins@sfr.fr>
2021-10-10 16:02:25 +0200

    Added a class that contains the name of all permissions

---
+++ src/main/java/me/crylonz/Permission.java
@@ -0,0 +1,20 @@
+package me.crylonz;
+
+public class Permission {
+
+    private static final String _DC = "deadchest";
+
+    public static final String CHESTPASS = _DC + ".chestPass";
+    public static final String GET = _DC + ".get";
+    public static final String GENERATE = _DC + ".generate";
+    public static final String GIVEBACK = _DC + ".giveback";
+    public static final String INFINITY_CHEST = _DC + ".infityChest";
+
+    public static final String REMOVE_OWN = _DC + ".remove.own";
+    public static final String REMOVE_OTHER = _DC + ".remove.other";
+
+    public static final String LIST_OWN = _DC + ".list.own";
+    public static final String LIST_OTHER = _DC + ".list.other";
+
+    public static final String ADMIN = _DC + ".admin";
+}
