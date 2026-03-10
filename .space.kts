ed31ac5596d0ed147030d7a8b27df1a42bf041e8
Arthur Pavarino <5781079+apavarino@users.noreply.github.com>
2026-03-10 19:51:59 +0100

    Clean old CI file

--- .space.kts
+++
@@ -1,8 +0,0 @@
-job("Build and publish") {
-    container(displayName = "Run publish script", image = "gradle") {
-        kotlinScript { api ->
-            api.gradlew("build")
-            api.gradlew("publish")
-        }
-    }
-}
